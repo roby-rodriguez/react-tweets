cc3643d09161686f6e7d237a469ba6972baf76a5
Roby Rodriguez <andrey.roba@gmail.com>
2016-12-24 14:36:23 +0200

    adjust config for c9

--- app/config/index.js
+++ app/config/index.js
@@ -3,5 +3,6 @@ if (process.env.C9_PROJECT)
     env = require('./remote.json')
 else
     env = require('./local.json')
+const auth = require('./auth.json')
 const shared = require('./shared.json')
-module.exports = Object.assign({}, env, shared)
+module.exports = Object.assign({}, env, auth, shared)
